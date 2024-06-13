45b9fe1b85fa2d4d66620aba03c2742df1e6c2ea
Suzanne Vandeweerdt <suzanne.vandeweerdt@student.ucll.be>
2024-06-13 23:26:01 +0200

    01 up to coins

--- 01-arithmetic/23-assignment-coins/student.py
+++ 01-arithmetic/23-assignment-coins/student.py
@@ -1,1 +1,5 @@
-# write your code here
+def coins(amount):
+    coins_5 = amount//5
+    coins_2 = (amount - 5*coins_5)//2
+    coins_1 = amount - 5*coins_5 - 2*coins_2
+    return coins_5 + coins_2 + coins_1
